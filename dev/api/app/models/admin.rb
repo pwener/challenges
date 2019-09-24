958fc0f55275af4d3e7552fb60fd75fe112e4a05
Phelipe <phwener@gmail.com>
2019-09-24 07:43:25 -0300

    Add more test cases

--- dev/api/app/models/admin.rb
+++ dev/api/app/models/admin.rb
@@ -3,4 +3,5 @@ class Admin < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :login, presence: true, length: { in: 2..25 }
 end
--- dev/api/spec/models/admin_spec.rb
+++ dev/api/spec/models/admin_spec.rb
@@ -8,6 +8,26 @@ RSpec.describe Admin, type: :model do
   it { should respond_to(:email) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
-
   it { should be_valid }
+
+  describe "when email is not present" do
+    before { @admin.email = " " }
+    it { should_not be_valid }
+  end
+
+  describe "when email is duplicated" do
+    first = Admin.new(email: 'adm@adm.com')
+    duplicated = Admin.new(email: 'adm@adm.com')
+    it { duplicated.should_not be_valid }
+  end
+
+  describe "when password is short" do
+    before { @admin.password = "adm" }
+    it { should_not be_valid }
+  end
+
+  describe "when login is invalid" do
+    before { @admin.login = " " }
+    it { should_not be_valid }
+  end
 end
