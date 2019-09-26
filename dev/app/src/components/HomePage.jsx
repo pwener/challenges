b464ccb5b494c7daa4096c34905332962e47c981
Phelipe <phwener@gmail.com>
2019-09-26 09:49:25 -0300

    Just a sample to homepage

--- dev/app/src/components/HomePage.jsx
+++ dev/app/src/components/HomePage.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import {
-  Container
+  Card,
+  Container,
 } from "semantic-ui-react";
+import ThreatCard from './ThreatCard';
+
+const generateLoc = () => "lat: -5.836597, lng: -35.236007"
+
+const monsters = [
+  { location: generateLoc(), dangerLevel: 'S', monsterName: 'Silver Dragon' },
+  { location: generateLoc(), dangerLevel: 'G', monsterName: 'Black Dragon' },
+  { location: generateLoc(), dangerLevel: 'C', monsterName: 'Giant Mamba' },
+  { location: generateLoc(), dangerLevel: 'W', monsterName: 'Meteour' },
+  { location: generateLoc(), dangerLevel: 'G', monsterName: 'Zombies' },
+];
 
 const HomePage = (props) => (
-  <Container>
-    <h1>Hello world</h1>
+  <Container textAlign="center">
+    <Card.Group centered>
+      { monsters.map((m, i) => <ThreatCard threat={m} /> ) }
+    </Card.Group>
   </Container>
 );
 
---
+++ dev/app/src/components/ThreatCard.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Card, Image } from 'semantic-ui-react';
+
+const ThreatCard = ({ threat }) => (
+  <Card>
+    <Card.Content>
+      <Image
+        floated='right'
+        size='mini'
+        src='/images/avatar/large/steve.jpg'
+      />
+      <Card.Header>{threat.monsterName}</Card.Header>
+      <Card.Meta>{threat.location}</Card.Meta>
+      <Card.Description>
+      </Card.Description>
+    </Card.Content>
+  </Card>
+)
+
+export default ThreatCard;
