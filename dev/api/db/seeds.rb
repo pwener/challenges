b4742b5597dafcb7cb943da749db0766899d8b32
Phelipe <phwener@gmail.com>
2019-09-28 02:14:50 -0300

    Add dynamic seeds for heroes

--- dev/api/db/seeds.rb
+++ dev/api/db/seeds.rb
@@ -6,3 +6,14 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Admin.create(login: 'admin', email: 'adm@adm.com', password: 'admin123', password_confirmation: 'admin123')
+
+heroes = ['Saitama', 'Goku', 'Vegeta', 'Naruto', 'Sasuke', 'Drow Ranger', 'Axe', 'Mortred', 'Andromeda', 'Sandman', 'Zero']
+ranks = Hero.ranks.keys
+
+def range
+  rand * (1-0.00001) + 0.00001
+end
+
+heroes.each do |e|
+  Hero.create(name: e, rank: ranks.sample, location_attributes: { longitude: range(), latitude: range()})
+end
