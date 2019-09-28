20b240c83b0fdf41f726bd7f471723f80ffd4094
Phelipe <phwener@gmail.com>
2019-09-28 17:20:27 -0300

    Just comments

--- dev/api/app/controllers/batle_pool.rb
+++ dev/api/app/controllers/batle_pool.rb
@@ -20,6 +20,7 @@ class BatlePool
           b.save
         end
         puts "Have #{@batles.size} batles"
+        # waits 10 seconds
         sleep(10)
       end
     end
@@ -36,6 +37,7 @@ class BatlePool
     else
       loop do
         release(threat)
+        # waits 5 seconds
         sleep(5)
       end
     end
