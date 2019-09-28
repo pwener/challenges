0fd21357fe084008fa0b005f19cb128a1ed5dc59
Phelipe <phwener@gmail.com>
2019-09-28 02:29:56 -0300

    Fix axios instance to reload when 401

--- dev/app/src/helpers/axios.js
+++ dev/app/src/helpers/axios.js
@@ -1,10 +1,26 @@
 import axios from 'axios';
 import authHeader from './authHeader';
 
-const api = axios.create({
-  baseURL: 'http://localhost:3000',
-});
+const createAxiosInstance = (token) => {
+  const api = axios.create({
+    baseURL: 'http://localhost:3000',
+  });
 
-api.defaults.headers.common.Authorization = authHeader();
+  api.interceptors.response.use((response) => response,
+    (error) => {
+      if (error && error.response) {
+        if (error.response.status === 401) {
+          window.location.reload(true);
+        }
+      }
+      return Promise.reject(error);
+    });
+
+  api.defaults.headers.common.Authorization = token;
+
+  return api;
+};
+
+const api = createAxiosInstance(authHeader());
 
 export default api;
