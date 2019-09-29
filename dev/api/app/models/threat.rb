e857a336d1099069f7c530b90c62cb6f637d0c2c
Phelipe <phwener@gmail.com>
2019-09-29 14:56:56 -0300

    Fix validation to name

--- dev/api/app/models/threat.rb
+++ dev/api/app/models/threat.rb
@@ -2,7 +2,7 @@ class Threat < ApplicationRecord
   has_one :location
   accepts_nested_attributes_for :location, :allow_destroy => true
   enum rank: { g: 0, s: 1, c: 2, w: 3 }
-  validates :name, uniqueness: true, length: { in: 2..20 }
+  validates :name, uniqueness: true, length: { in: 2..40 }
   validates :rank, presence: true
   validates :location, presence: true
 end
